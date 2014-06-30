964b69b561e58a9e1a086d2f22967454f1c9a96a
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2014-06-30 17:25:40 +0200

    Cleanup setup.py
    Cleanup versioning and update various fields

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from fancytree import VERSION
+import fancytree
 import os
 
 setup(name='django-fancytree',
-      version=VERSION,
+      version=fancytree.__version__,
       description='Django forms widget that uses Fancytree to display tree data',
       author='Riccardo Magliocchetti',
       author_email='riccardo.magliocchetti@gmail.com',
@@ -13,14 +13,19 @@ setup(name='django-fancytree',
       packages=['fancytree',],
       keywords=['django', 'fancytree', 'mptt', 'tree'],
       classifiers=[
-          'Development Status :: 1 - Planning',
           'Programming Language :: Python',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: BSD License',
           'Operating System :: OS Independent',
+          'Environment :: Web Environment',
           'Framework :: Django',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
       ],
       long_description=open(
           os.path.join(os.path.dirname(__file__), 'README.rst'),
       ).read().strip(),
+      include_package_data=True,
+      install_required=[
+          'Django',
+      ]
 )
